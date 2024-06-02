481412597669e762d5c2b6c167771f0d219aa49d
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-06-01 20:42:11 -0500

    feat: Se añade page accordion

--- app/dashboard/accordion/page.tsx
+++ app/dashboard/accordion/page.tsx
@@ -1,7 +1,39 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Page() {
+  const items = [
+    {
+      id: "item-1",
+      question: "¿Es accesible?",
+      answer: "Sí. Se adhiere al patrón de diseño WAI-ARIA.",
+    },
+    {
+      id: "item-2",
+      question: "¿Tiene estilo?",
+      answer:
+        "Sí. Viene con estilos predeterminados que coinciden con los demás componentes estético.",
+    },
+    {
+      id: "item-3",
+      question: "¿Está animado?",
+      answer:
+        "Sí. Está animado de forma predeterminada, pero puedes desactivarlo si lo prefieres.",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Accordion Page</h1>
-    </div>
+    <Accordion type="single" collapsible className="w-full">
+      {items.map((item) => (
+        <AccordionItem value={item.id} key={item.id}>
+          <AccordionTrigger>{item.question}</AccordionTrigger>
+          <AccordionContent>{item.answer}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
   );
 }
