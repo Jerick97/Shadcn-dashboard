dd57fd8f06fcc9ee7ca760506bb0591df8b1ba49
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-06-05 19:04:22 -0500

    feat: Se añade nueva Variante success al componente Alert

--- app/dashboard/alert/page.tsx
+++ app/dashboard/alert/page.tsx
@@ -1,7 +1,33 @@
+import { RocketIcon, CheckIcon } from "@radix-ui/react-icons";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-3">
+      <Alert>
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Default</AlertTitle>
+        <AlertDescription>
+          Puede agregar componentes a su aplicación usando cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant="destructive">
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Destructive</AlertTitle>
+        <AlertDescription>
+          Puede agregar componentes a su aplicación usando cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant="success">
+        <CheckIcon className="h-4 w-4" />
+        <AlertTitle>Success</AlertTitle>
+        <AlertDescription>
+          Puede agregar componentes a su aplicación usando cli.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
